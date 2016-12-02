556cc082a3dbab6bf576edbc1e34ccbf68cbb04c
Diego Neira <dnfneira@gmail.com>
2016-12-02 14:19:15 +0100

    Añadida Vista Vehiculos

---
+++ src/main/java/es/uvigo/webapp/VehiculosVM.java
@@ -0,0 +1,70 @@
+package es.uvigo.webapp;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+
+import es.uvigo.Conductor;
+import es.uvigo.TransactionUtils;
+import es.uvigo.Vehiculo;
+import es.uvigo.webapp.util.DesktopEntityManagerManager;
+
+public class VehiculosVM {
+	
+	private Vehiculo currentVehiculo = null;
+	
+	public Vehiculo getCurrentVehiculo() {
+		return currentVehiculo;
+	}
+	
+	public List<Conductor> getConductores(){
+		EntityManager em = DesktopEntityManagerManager.getDesktopEntityManager();
+		return em.createQuery("SELECT c FROM Conductor c", Conductor.class).getResultList();
+	}
+	
+	public List<Vehiculo> getVehiculos(){
+		EntityManager em = DesktopEntityManagerManager.getDesktopEntityManager();
+		return em.createQuery("SELECT v FROM Vehiculo v", Vehiculo.class).getResultList();
+	}
+	
+	@Command
+	@NotifyChange("currentVehiculo")
+	public void newVehiculo() {
+		this.currentVehiculo = new Vehiculo();
+	}
+	
+	@Command
+	@NotifyChange("vehiculos")
+	public void delete(@BindingParam("v") Vehiculo vehiculo) {
+		EntityManager em = DesktopEntityManagerManager.getDesktopEntityManager();
+		TransactionUtils.doTransaction(em, __ -> {
+			em.remove(vehiculo);
+		});
+	}
+	
+	@Command
+	@NotifyChange("currentVehiculo")
+	public void edit(@BindingParam("v") Vehiculo vehiculo) {
+		this.currentVehiculo= vehiculo;
+	}
+	
+	@Command
+	@NotifyChange({"vehiculos", "currentVehiculo"})
+	public void save() {
+		EntityManager em = DesktopEntityManagerManager.getDesktopEntityManager();
+		TransactionUtils.doTransaction(em, __ -> {
+			em.persist(this.currentVehiculo);
+		});
+		this.currentVehiculo= null;
+	}
+	
+	@Command
+	@NotifyChange("currentVehiculo")
+	public void cancel() {
+		this.currentVehiculo= null;
+	}
+}
